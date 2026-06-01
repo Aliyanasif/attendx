cc71cab5263f30de2db4a3fe25513b562e787426
Aliyanasif <150242892+Aliyanasif@users.noreply.github.com>
2026-06-01 11:45:24 +0500

    Update attendance and navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
 import { SidebarProvider } from "@/context/SidebarContext";
+import { Analytics } from "@vercel/analytics/next"
 import { Toaster } from "react-hot-toast"; 
 import SplashScreen from "@/components/SplashScreen";
 import LayoutLogic from "@/components/LayoutLogic"; // Hum isay client component banayenge
